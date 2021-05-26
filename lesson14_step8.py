d9f5c1e664c88ecec35f364b995bfa91fdaba467
dizzy1978 <dizzy1978@gmail.com>
2021-05-26 20:53:53 +0300

    assert test function

---
+++ lesson14_step8.py
@@ -0,0 +1,24 @@
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+
+
+
+
+#catalog_text = self.catalog_link.text # считываем текст и записываем его в переменную
+#assert catalog_text == "Каталог", f"Wrong language, got {catalog_text} instead of 'Каталог'"
+
+
+#Пример 1:
+#str1 = "one"
+#str2 = "two"
+#str3 = "three"
+#print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+#Итог выполнения выражений в интерпретаторе:
+#Let's count together: one, then goes two, and then three
+
+#Пример 2:
+#actual_result = "abrakadabra"
+#f"Wrong text, got {actual_result}, something wrong"
+#Итог выполнения выражений в интерпретаторе:
+#Wrong text, got abrakadabra, something wrong
