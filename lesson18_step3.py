ab8af88fef845a7ac5ea32968adf1151ef8d7cc4
dizzy1978 <dizzy1978@gmail.com>
2021-05-28 23:02:38 +0300

    homework by course's block

--- lesson18_step3.py
+++ lesson18_step3.py
@@ -37,17 +37,20 @@ class TestMainPage1():
         text_area = browser.find_element_by_xpath('//textarea')
         y = str(math.log(int(time.time())))
         text_area.send_keys(y)
-        #button = browser.find_element_by_xpath('//button[text()="Отправить"]')
         button = WebDriverWait(browser, 5).until(
             EC.element_to_be_clickable((By.XPATH, '//button[text()="Отправить"]'))
         )
         button.click()
+        #time.sleep(1)
+        message = browser.find_element_by_class_name("smart-hints__hint")
+        print(message.text)
+        assert "Correct!" in message.text, "Это часть послания!"
 
 
 
 
 
-
+        # <pre class ="smart-hints__hint" > Correct! < / pre >
         #print("Вычисленное значение %s", y)
         #print("Проверяемый URL %s" % linka)
         #browser.find_element_by_css_selector("#login_link")
