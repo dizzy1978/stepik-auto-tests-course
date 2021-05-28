5247f10ad11e6c9fde3d24370307d3991248dc4f
dizzy1978 <dizzy1978@gmail.com>
2021-05-28 21:28:28 +0300

    homework by course's block

--- lesson18_step3.py
+++ lesson18_step3.py
@@ -6,12 +6,12 @@ from selenium import webdriver
 links = [
     ("https://stepik.org/lesson/236895/step/1"),
     ("https://stepik.org/lesson/236896/step/1"),
-    ("https://stepik.org/lesson/236897/step/1"),
-    ("https://stepik.org/lesson/236898/step/1"),
-    ("https://stepik.org/lesson/236899/step/1"),
-    ("https://stepik.org/lesson/236903/step/1"),
-    ("https://stepik.org/lesson/236904/step/1"),
-    ("https://stepik.org/lesson/236905/step/1")
+    #("https://stepik.org/lesson/236897/step/1"),
+    #("https://stepik.org/lesson/236898/step/1"),
+    #("https://stepik.org/lesson/236899/step/1"),
+    #("https://stepik.org/lesson/236903/step/1"),
+    #("https://stepik.org/lesson/236904/step/1"),
+    #("https://stepik.org/lesson/236905/step/1")
  ]
 
 @pytest.fixture(scope="function")
@@ -30,6 +30,12 @@ class TestMainPage1():
     def test_guest_should_see_login_link(self, browser, link):
         linka = f"{link}"
         browser.get(linka)
+        #text_area = browser.find_element_by_id("ember93")
+        y = str(math.log(int(time.time())))
+        #text_area.send_keys(y)
+        print("Вычисленное значение %s", y)
+
+
+        #print("Проверяемый URL %s" % linka)
         #browser.find_element_by_css_selector("#login_link")
-        print("Проверяемый URL %s" % linka)
 
