e03d29cc0cccd3a5fb9cbb5615fca0dbd3ccdadf
dizzy1978 <dizzy1978@gmail.com>
2021-05-28 22:18:10 +0300

    homework by course's block

--- lesson18_step3.py
+++ lesson18_step3.py
@@ -1,17 +1,20 @@
 import time
 import math
 import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 links = [
     ("https://stepik.org/lesson/236895/step/1"),
     ("https://stepik.org/lesson/236896/step/1"),
-    #("https://stepik.org/lesson/236897/step/1"),
-    #("https://stepik.org/lesson/236898/step/1"),
-    #("https://stepik.org/lesson/236899/step/1"),
-    #("https://stepik.org/lesson/236903/step/1"),
-    #("https://stepik.org/lesson/236904/step/1"),
-    #("https://stepik.org/lesson/236905/step/1")
+    ("https://stepik.org/lesson/236897/step/1"),
+    ("https://stepik.org/lesson/236898/step/1"),
+    ("https://stepik.org/lesson/236899/step/1"),
+    ("https://stepik.org/lesson/236903/step/1"),
+    ("https://stepik.org/lesson/236904/step/1"),
+    ("https://stepik.org/lesson/236905/step/1")
  ]
 
 @pytest.fixture(scope="function")
@@ -34,10 +37,12 @@ class TestMainPage1():
         text_area = browser.find_element_by_xpath('//textarea')
         y = str(math.log(int(time.time())))
         text_area.send_keys(y)
-        time.sleep(1)
-        button = browser.find_element_by_xpath('//button[text()="Отправить"]')
+        #button = browser.find_element_by_xpath('//button[text()="Отправить"]')
+        button = WebDriverWait(browser, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[text()="Отправить"]'))
+        )
         button.click()
-        time.sleep(3)
+
 
 
 
