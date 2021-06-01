fb118a80b59fb7fbe4dcf0ca1fc53aa0adb175cf
dizzy1978 <dizzy1978@gmail.com>
2021-06-01 21:07:16 +0300

    re

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time # Для проверки "глазами"
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_button_is_present(browser):
     browser.get(link)
     time.sleep(2) # Для проверки "глазами"
     txt_button = browser.find_element_by_xpath('//*[@id="add_to_basket_form"]/button').text
