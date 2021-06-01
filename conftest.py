3ef20ca2af6668f5f6dce96e96d2dd60366078dc
dizzy1978 <dizzy1978@gmail.com>
2021-06-01 16:45:55 +0300

    Параметризация - задание нескольких браузеров как параметров и/или браузера по-умолчанию.

--- conftest.py
+++ conftest.py
@@ -1,13 +1,24 @@
 import pytest
 from selenium import webdriver
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    #parser.addoption('--browser_name', action='store', default="chrome",   # browser by default set
+    #                 help="Choose browser: chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(10)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
