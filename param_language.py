2777bc0551ab5ed6841bfd67f87228bf35c15403
dizzy1978 <dizzy1978@gmail.com>
2021-06-01 17:07:43 +0300

    parametrise languages in firefox/chrome

---
+++ param_language.py
@@ -0,0 +1,14 @@
+#Чтобы указать язык браузера с помощью WebDriver, используйте класс Options и
+# метод add_experimental_option, как указано в примере ниже:
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+#Для Firefox объявление нужного языка будет выглядеть немного иначе:
+
+fp = webdriver.FirefoxProfile()
+fp.set_preference("intl.accept_languages", user_language)
+browser = webdriver.Firefox(firefox_profile=fp)
