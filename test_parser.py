e110a539293df84f8ee5be62b7d75578d4ce57f1
dizzy1978 <dizzy1978@gmail.com>
2021-06-01 20:50:42 +0300

    re

--- test_parser.py
+++ test_parser.py
@@ -1,6 +1,9 @@
-link = "http://selenium1py.pythonanywhere.com/"
+ link = "http://selenium1py.pythonanywhere.com/"
+import time
+
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
+    time.sleep(20)
     browser.find_element_by_css_selector("#login_link")
